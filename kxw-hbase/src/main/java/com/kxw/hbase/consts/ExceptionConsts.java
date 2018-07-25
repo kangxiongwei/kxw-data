8016f6c9030b599e5d5e7bad2744cc88dbf86bc5
亢雄伟 <kangxiongwei@meituan.com>
2018-07-25 13:44:52 +0800

    利用JavaAPI进程HBase的建表、删表、插入数据、查询数据操作

--- kxw-hbase/src/main/java/com/kxw/hbase/consts/ExceptionConsts.java
+++ kxw-hbase/src/main/java/com/kxw/hbase/consts/ExceptionConsts.java
@@ -8,5 +8,5 @@ public class ExceptionConsts {
     public static final String GET_CLOSE_ADMIN = "获取或关闭HBase的Admin连接异常";
     public static final String GET_CLOSE_CONNECTION = "获取或关闭HBase的Connection连接异常";
 
-
+    private ExceptionConsts() {}
 }
--- kxw-hbase/src/test/java/com/kxw/hbase/BaseDaoTest.java
+++ kxw-hbase/src/test/java/com/kxw/hbase/BaseDaoTest.java
@@ -2,6 +2,9 @@ package com.kxw.hbase;
 
 import com.kxw.hbase.dao.HBaseAdminDao;
 import com.kxw.hbase.dao.HBaseDao;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Created by kangxiongwei on 2018/7/25 13:25
@@ -14,4 +17,18 @@ public class BaseDaoTest {
     String tableName = "h_user";
     String[] columnFamilies = new String[] {"base_info", "ext_info"};
 
+    @Before
+    public void setUp() {
+        System.out.println("初始化数据");
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("销毁数据");
+    }
 }
